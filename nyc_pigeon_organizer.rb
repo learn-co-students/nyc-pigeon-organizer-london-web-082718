1341305c5effbe829c0cc570cfb27a0b5f1125a5
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-16 15:13:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = []
 
-  data.each do |key, value| #change for data
+  data.each do |key, value|
     value.each do |key2, value2|
         pigeon_list << value2
     end
@@ -10,18 +10,18 @@ def nyc_pigeon_organizer(data)
 
   pigeon_list = pigeon_list.flatten.uniq
 
-  pigeon_list = pigeon_list.collect { |item| [item, [data]] } #change for data
+  pigeon_list = pigeon_list.collect { |item| [item, [data]] }
 
-#  pigeon_list.each {|k,v| v.delete(:two) if k == :clubs}
   pigeon_list.each do |key, value|
     value[0].each do |key2, value2|
       value2.each do |key3, value3|
+
         binding.pry
       #  if value3.any? != key
         #  key3.delete
       #  end
+      end
+    end
   end
-end
-end
 end
 ##
