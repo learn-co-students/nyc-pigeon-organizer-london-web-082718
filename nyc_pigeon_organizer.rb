fb1e87eb6861c13e3f28b694b65d7a9a6babf040
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-16 18:00:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,40 +1,17 @@
 require "pry"
 def nyc_pigeon_organizer(data)
-  pigeon_list = []
+  pigeon_list = {}
 
-  data.each do |key, value|
-    value.each do |key2, value2|
-        pigeon_list << value2
+  data.each do |info, options|
+    options.each do |option, names|
+      names.each do |name|
 
-    end
-  end
-
-  pigeon_list = pigeon_list.flatten.uniq
-
-  hash = Hash.new
-  pigeon_list.each do |name|
-    hash[name] = {}
-  end
-
-  hash.each do |item, value|
-    hash[item] = data
-  end
-
-  hash.each do |key, value|
-    value.each do |key2, value2|
-      value2.each do |key3, value3|
-
-
-        if !value3.include?(key)
-          #key3.delete # hash[key][key2].delete_if {|key3| !value3.include?(key)}
-          hash[key][key2].delete(key3)
-        end
+      pigeon_list[name] ||= {}
+      pigeon_list[name][info] ||= []
+      pigeon_list[name][info] << option.to_s
 
       end
-
     end
-
   end
-  binding.pry
+  pigeon_list
 end
-##
